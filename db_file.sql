4e30e9dfd9625127f8347759b1f44789bb9a69ab
Hamza Ennour <mouhamedkhalil.jaber@esprit.tn>
2023-09-02 23:39:23 +0100

    updating backup file

--- db_file.sql
+++ db_file.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Sep 01, 2023 at 11:45 PM
+-- Generation Time: Sep 02, 2023 at 10:38 PM
 -- Server version: 5.7.36
 -- PHP Version: 8.0.13
 
@@ -20,6 +20,92 @@ SET time_zone = "+00:00";
 --
 -- Database: `projet`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `favoris`
+--
+
+DROP TABLE IF EXISTS `favoris`;
+CREATE TABLE IF NOT EXISTS `favoris` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idUser` int(11) NOT NULL,
+  `idFormation` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_userF_id` (`idUser`),
+  KEY `fk_formationF_id` (`idFormation`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `favoris`
+--
+
+INSERT INTO `favoris` (`id`, `idUser`, `idFormation`) VALUES
+(2, 3, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `formations`
+--
+
+DROP TABLE IF EXISTS `formations`;
+CREATE TABLE IF NOT EXISTS `formations` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) NOT NULL,
+  `description` varchar(255) NOT NULL,
+  `startDate` varchar(255) NOT NULL,
+  `endDate` varchar(255) NOT NULL,
+  `typeId` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_type_id` (`typeId`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `formations`
+--
+
+INSERT INTO `formations` (`id`, `title`, `description`, `startDate`, `endDate`, `typeId`) VALUES
+(1, 'tt', 'bbbbbb', '2023-08-28', '2023-08-29', 2),
+(3, 'formation Java', 'test java', '2023-09-22', '2023-09-29', 5);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `participate`
+--
+
+DROP TABLE IF EXISTS `participate`;
+CREATE TABLE IF NOT EXISTS `participate` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `idUser` int(11) NOT NULL,
+  `idFormation` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_userP_id` (`idUser`),
+  KEY `fk_formationP_id` (`idFormation`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `types`
+--
+
+DROP TABLE IF EXISTS `types`;
+CREATE TABLE IF NOT EXISTS `types` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `types`
+--
+
+INSERT INTO `types` (`id`, `name`) VALUES
+(2, 'nodejsss'),
+(5, 'Java');
 
 -- --------------------------------------------------------
 
@@ -46,6 +132,30 @@ INSERT INTO `users` (`id`, `username`, `password`, `role`, `date_join`) VALUES
 (2, 'hamza', '$2y$10$xeJhSgpvTYWxhEiEsFBLLOSbwsWKX8cMuAKc4rFLiJtIeQDBOUvw6', 'admin', '2023-08-30 16:31:05'),
 (3, 'amine', '$2y$10$tJHC4vkpuCV4Aec5qhdtbOrPufbQ9LIZaktQHZRj4afU0S1z7B68K', 'student', '2023-09-01 11:37:03'),
 (4, 'khalil', '$2y$10$IX.reUyMarZkV5fmoh59..nSdsNsN01kqm9VF4mGXWzsokVspwkEy', 'teacher', '2023-09-01 11:37:27');
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `favoris`
+--
+ALTER TABLE `favoris`
+  ADD CONSTRAINT `fk_formationF_id` FOREIGN KEY (`idFormation`) REFERENCES `formations` (`id`),
+  ADD CONSTRAINT `fk_userF_id` FOREIGN KEY (`idUser`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `formations`
+--
+ALTER TABLE `formations`
+  ADD CONSTRAINT `fk_type_id` FOREIGN KEY (`typeId`) REFERENCES `types` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `participate`
+--
+ALTER TABLE `participate`
+  ADD CONSTRAINT `fk_formationP_id` FOREIGN KEY (`idFormation`) REFERENCES `formations` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `fk_userP_id` FOREIGN KEY (`idUser`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
